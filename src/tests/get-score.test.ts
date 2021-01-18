24c1c4dbdfde74dfb203ca9e11153e646f2f84f5
arubtsov <arseniy.rubtsov@gmail.com>
2021-01-19 00:16:09 +0300

    refactor getScore to accept Frame[] instead of Game

--- src/tests/get-score.test.ts
+++ src/tests/get-score.test.ts
@@ -1,7 +1,9 @@
 import { getScore } from '../utils/get-score';
-import { createGame, addFrame } from '../utils/constructors';
+import { addFrame } from '../utils/constructors';
+import { Frame } from '../types';
 
 describe('getScore()', () => {
+    let frames: Frame[] = [];
     const playerOne = {
         name: 'Jesus',
         gamesWon: 0
@@ -14,107 +16,90 @@ describe('getScore()', () => {
     const onePLayer = [playerOne];
     const twoPlayers = [playerOne, playerTwo];
 
+    beforeEach(() => {
+        frames = [];
+    });
 
     it('Should return 0 if no frames are played yet', () => {
-        const game = createGame();
-
-        expect(getScore(playerOne.name, game)).toEqual(0);
+        expect(getScore(playerOne.name, frames)).toEqual(0);
     });
 
     it('Should return sum of attempts for a player', () => {
-        let game = createGame();
+        addFrame(frames, onePLayer, [[2, 5]]);
+        addFrame(frames, onePLayer, [[3, 7]]);
 
-        game = addFrame(game, onePLayer, [[2, 5]]);
-        game = addFrame(game, onePLayer, [[3, 7]]);
-
-        expect(getScore(playerOne.name, game)).toEqual(17);
+        expect(getScore(playerOne.name, frames)).toEqual(17);
     });
 
     it('Should distinct players while calculating the score', () => {
-        let game = createGame();
+        addFrame(frames, twoPlayers, [[2, 5], [1, 4]]);
+        addFrame(frames, twoPlayers, [[3, 7], [2, 6]]);
 
-        game = addFrame(game, twoPlayers, [[2, 5], [1, 4]]);
-        game = addFrame(game, twoPlayers, [[3, 7], [2, 6]]);
-
-        expect(getScore(playerOne.name, game)).toEqual(17);
-        expect(getScore(playerTwo.name, game)).toEqual(13);
+        expect(getScore(playerOne.name, frames)).toEqual(17);
+        expect(getScore(playerTwo.name, frames)).toEqual(13);
     });
 
     it('Should take strikes into account', () => {
-        let game = createGame();
+        addFrame(frames, onePLayer, [[10, 0]]);
+        addFrame(frames, onePLayer, [[3, 6]]);
+        addFrame(frames, onePLayer, [[1, 0]]);
 
-        game = addFrame(game, onePLayer, [[10, 0]]);
-        game = addFrame(game, onePLayer, [[3, 6]]);
-        game = addFrame(game, onePLayer, [[1, 0]]);
-
-        expect(getScore(playerOne.name, game)).toEqual(29);
+        expect(getScore(playerOne.name, frames)).toEqual(29);
     });
 
     it('Regression: Strike in last but one frame', () => {
-        let game = createGame();
+        addFrame(frames, onePLayer, [[10, 0]]);
+        addFrame(frames, onePLayer, [[3, 6]]);
 
-        game = addFrame(game, onePLayer, [[10, 0]]);
-        game = addFrame(game, onePLayer, [[3, 6]]);
-
-        expect(getScore(playerOne.name, game)).toEqual(28);
+        expect(getScore(playerOne.name, frames)).toEqual(28);
     });
 
     it('Should correctly score two Strikes in a row', () => {
-        let game = createGame();
+        addFrame(frames, onePLayer, [[10, 0]]);
+        addFrame(frames, onePLayer, [[10, 0]]);
+        addFrame(frames, onePLayer, [[4, 0]]);
+        addFrame(frames, onePLayer, [[2, 0]]);
 
-        game = addFrame(game, onePLayer, [[10, 0]]);
-        game = addFrame(game, onePLayer, [[10, 0]]);
-        game = addFrame(game, onePLayer, [[4, 0]]);
-        game = addFrame(game, onePLayer, [[2, 0]]);
-
-        expect(getScore(playerOne.name, game)).toEqual(44);
+        expect(getScore(playerOne.name, frames)).toEqual(44);
     });
 
     it('Should correctly score Strike followed by a miss', () => {
-        let game = createGame();
+        addFrame(frames, onePLayer, [[10, 0]]);
+        addFrame(frames, onePLayer, [[0, 5]]);
+        addFrame(frames, onePLayer, [[6, 0]]);
 
-        game = addFrame(game, onePLayer, [[10, 0]]);
-        game = addFrame(game, onePLayer, [[0, 5]]);
-        game = addFrame(game, onePLayer, [[6, 0]]);
-
-        expect(getScore(playerOne.name, game)).toEqual(26);
+        expect(getScore(playerOne.name, frames)).toEqual(26);
     });
 
     it('Should correctly score Spare', () => {
-        let game = createGame();
+        addFrame(frames, onePLayer, [[8, 2]]);
+        addFrame(frames, onePLayer, [[2, 0]]);
 
-        game = addFrame(game, onePLayer, [[8, 2]]);
-        game = addFrame(game, onePLayer, [[2, 0]]);
-
-        expect(getScore(playerOne.name, game)).toEqual(14);
+        expect(getScore(playerOne.name, frames)).toEqual(14);
     });
 
     it('Should correctly score perfect game', () => {
-        let game = createGame();
-
         for(let index = 0; index < 9; index++)
-            game = addFrame(game, onePLayer, [[10, 0]]);
+            addFrame(frames, onePLayer, [[10, 0]]);
 
-        game = addFrame(game, onePLayer, [[10, 10, 10]]);
+        addFrame(frames, onePLayer, [[10, 10, 10]]);
 
-        expect(getScore(playerOne.name, game)).toEqual(300);
+        expect(getScore(playerOne.name, frames)).toEqual(300);
     });
 
     it('Should correctly score given examples',() => {
-        let game = createGame();
+        addFrame(frames, twoPlayers, [[8, 0], [8, 2]]);
+        addFrame(frames, twoPlayers, [[7, 0], [9, 0]]);
+        addFrame(frames, twoPlayers, [[5, 3], [4, 4]]);
+        addFrame(frames, twoPlayers, [[9, 1], [7, 2]]);
+        addFrame(frames, twoPlayers, [[9, 1], [9, 0]]);
+        addFrame(frames, twoPlayers, [[10, 0], [10, 0]]);
+        addFrame(frames, twoPlayers, [[8, 0], [10, 0]]);
+        addFrame(frames, twoPlayers, [[5, 1], [8, 0]]);
+        addFrame(frames, twoPlayers, [[3, 7], [3, 5]]);
+        addFrame(frames, twoPlayers, [[9, 0], [9, 1, 7]]);
 
-        game = addFrame(game, twoPlayers, [[8, 0], [8, 2]]);
-        game = addFrame(game, twoPlayers, [[7, 0], [9, 0]]);
-        game = addFrame(game, twoPlayers, [[5, 3], [4, 4]]);
-        game = addFrame(game, twoPlayers, [[9, 1], [7, 2]]);
-        game = addFrame(game, twoPlayers, [[9, 1], [9, 0]]);
-        game = addFrame(game, twoPlayers, [[10, 0], [10, 0]]);
-        game = addFrame(game, twoPlayers, [[8, 0], [10, 0]]);
-        game = addFrame(game, twoPlayers, [[5, 1], [8, 0]]);
-        game = addFrame(game, twoPlayers, [[3, 7], [3, 5]]);
-        game = addFrame(game, twoPlayers, [[9, 0], [9, 1, 7]]);
-
-        expect(getScore(playerOne.name, game)).toEqual(122);
-        expect(getScore(playerTwo.name, game)).toEqual(133);
+        expect(getScore(playerOne.name, frames)).toEqual(122);
+        expect(getScore(playerTwo.name, frames)).toEqual(133);
     });
 });
