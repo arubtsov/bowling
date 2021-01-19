4c40cf35208d3e088f91f1c721126871f8e64baa
arubtsov <arseniy.rubtsov@gmail.com>
2021-01-19 17:26:13 +0300

    install and configure redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,39 @@
 import { ThunkAction } from 'redux-thunk';
-import { configureStore, Action } from '@reduxjs/toolkit';
+import { configureStore, Action, getDefaultMiddleware  } from '@reduxjs/toolkit';
 import rootReducer, { RootState } from './rootReducer';
 
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-    reducer: rootReducer
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
+
+let persistor = persistStore(store);
+
+export { persistor };
 
 export type AppDispatch = typeof store.dispatch;
 
