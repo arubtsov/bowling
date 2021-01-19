4352be3c10928585f52d8aa109e97479474de2b8
arubtsov <arseniy.rubtsov@gmail.com>
2021-01-19 23:22:27 +0300

    extract scorePlayers function form gameSlice

--- src/features/game/gameSlice.ts
+++ src/features/game/gameSlice.ts
@@ -5,6 +5,7 @@ import { createGameFrame } from '../../utils/constructors';
 import { isDone } from '../../utils/predicates';
 import { getFrameScore } from '../../utils/get-score';
 import { updateRolls } from './update-rolls';
+import { scorePlayers } from './score-players';
 
 const initialState: Game = {
     frames: [],
@@ -75,33 +76,8 @@ const game = createSlice({
             if (gameFrame.isFinished && frameIndex === currentFrameIndex)
                 state.currentFrameIndex += 1;
 
-            if (state.isFinished) {
-                const lastFrameMap = frames[frames.length - 1].framesMap;
-                const scoring: Record<number, string[]> = {};
-
-                for (const [name, frame] of Object.entries(lastFrameMap)) {
-                    if (scoring[frame.total])
-                        scoring[frame.total].push(name);
-                    else
-                        scoring[frame.total] = [name];
-                }
-
-                Object.entries(scoring)
-                    .map<[number, string[]]>(
-                        ([score, names]) => [parseInt(score), names]
-                    )
-                    .sort((left, right) => {
-                        if (left[0] < right[0])
-                            return 1;
-                        else if (left[0] > right[0])
-                            return -1;
-
-                        return 0;
-                    }).forEach(([score, names], index) => {
-                        for (const name of names)
-                            state.placesWon[name] = index + 1;
-                    });
-            }
+            if (state.isFinished)
+                scorePlayers(state);
         }
     }
 });
